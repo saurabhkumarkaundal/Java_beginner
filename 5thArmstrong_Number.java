1bf51ac837e029fc27579d43128720e4c03a4de1
Saurabh Kumar Kaundal <80328201+saurabhkumarkaundal@users.noreply.github.com>
2022-01-16 19:42:44 +0530

    Update 5thArmstrong_Number.java

--- 5thArmstrong_Number.java
+++ 5thArmstrong_Number.java
@@ -1,1 +1,23 @@
+Task5:-Given a number N, you need to check whether the given number is Armstrong number or not. Now what is Armstrong number let us see below:
+A number is said to Armstrong if it is equal to sum of cube of its digits.
+Output
+You need to return "true" if the given number is an Armstrong number otherwise "false"
+  
+  static boolean isArmstrong(int N)
+    {
+        // your code here
+        int a = 0;
+        int n=N;
 
+        while (N != 0)
+        {
+           int reminder =N%10;
+            a=a+ (reminder*reminder*reminder);
+            N=N/10;
+        }
+
+        if(a == n)
+            return true;
+        else
+            return false;
+    }
